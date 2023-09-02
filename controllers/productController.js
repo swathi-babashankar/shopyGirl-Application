6a17c4875455771c32a4c5de35e2317f26e42716
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-09-02 15:02:28 +0530

    Added controllers

---
+++ controllers/productController.js
@@ -0,0 +1,262 @@
+import Mongoose from "mongoose";
+import product from "../model/tshirtSchema";
+import formidable from "formidable";
+import fs from "fs"
+import config from "../config/index.js";
+const Aggregate = require("mongoose")
+import {s3DeleteFile, s3FileUpload} from "../services/imageHandler.js"
+
+exports.createProduct = async (req, res) => {
+
+    const form = formidable({
+        multiples: true,
+        keepExtensions: true
+    })
+
+    form.parse(req, async function(err, fields, files){
+    
+        try{
+
+            if(err){
+                throw new Error("Something Went Wrong :(")
+            }
+
+            let productId = new Mongoose.Types.ObjectId().toHexString();
+            console.log(fields, files);
+
+            if(!fields.name || !fields.brand || !fields.category || !fields.price || !fields.discount){
+                throw new Error("Please fill necessary fields about the product")
+            }
+
+            let imgArrResp = Promise.all(
+                // to make sure the images coming are array
+                Object.keys(files).map( async (filekey, index) => {
+                    const elements = files[filekey];
+
+                    let data = fs.readFileSync(elements.filepath)
+
+                    const imgUpload = await s3FileUpload({
+                        bucketName: config.S3_BUCKET_NAME,
+                        key: `product/${productId}/photos_${index + 1}.png`,
+                        body: data,
+                        contentType: elements.mimetype
+                    })
+
+                    return{
+                        secure_url: imgUpload.Location
+                    }
+
+             })       
+            );
+    
+            let imgArray = await imgArrResp;
+            const productCreated = await product.create({
+                _id: productId,
+                images: imgArray,
+                ...fields
+            })
+
+        res.status(202).json({
+            success: true,
+            message: "Product created successfully",
+            productCreated
+        })
+
+        }
+
+        catch(err){
+            res.status(400).json({
+                success: false,
+                message: err.message
+            })
+
+        }
+    } 
+   
+    )
+    
+}
+
+exports.getProducts = async(req, res) => {
+
+    try{
+
+        const getAllprods = await product.find({})
+
+        res.status(202).json({
+            success: true,
+            message: "Products fetched successfully",
+            getAllprods
+        })
+
+    }
+
+    catch(err){
+
+        res.status(402).json({
+            success: false,
+            message: err.message
+        })
+
+
+
+    }
+}
+
+exports.getProductById = async(req, res) => {
+
+    try{
+
+        const {prodId} = req.params;
+        const getProdById = await product.findById({prodId})
+
+        if(getProdById){
+            res.status(202).json({
+                success: true,
+                message: "Products fetched successfully using productID",
+                getProdById
+            })
+        }
+
+    }
+
+    catch(err){
+
+        res.status(402).json({
+            success: false,
+            message: err.message
+        })
+
+    }
+}
+
+exports.updateProductById = async(req, res) => {
+    
+    try{
+
+        const {name, brand, category, price, discount} = req.body;
+        const {prodId} = req.params;
+
+        // how to ask user which field he want to update
+        // what if we want to update image
+        if(!name || !brand || !category || !price || discount){
+            throw new Error("Please fill necessary fields")
+        }
+
+        const productUpdated = await product.findByIdAndUpdate(prodId, {name, brand,
+            category, price, discount
+        })
+
+        res.status(202).json({
+            success: true,
+            message: "Product updated successfully",
+            productUpdated
+        })
+    }
+
+    catch(err){
+
+        res.status(400).json({
+            success: false,
+            message: err.message
+        })
+
+    }
+}
+
+// In case we want to change the price or apply discount only on particular brand
+exports.updateProductByBrand = async(req, res) => {
+
+    try{
+
+    const {brand, price, discount} = req.body;
+
+    if(!brand || !price || !discount){
+        throw new Error("Please fill all the fields if you want to update the product")
+    }
+
+    const productUpdated = await product.updateMany({brand}, {price, discount});
+
+    res.status(202).json({
+        success: true,
+        message: "Product updated successfully :)",
+        productUpdated
+    })
+    }
+
+    catch(err){
+
+        res.status(402).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+exports.deleteProduct = async(req, res) =>{
+
+    try{
+
+    const {prodId} = req.params;
+    const {img} = req.params.filename;
+    
+    if(!prodId){
+        throw new Error("Sorry Product ID not found")
+    }
+    
+    const imgDelete = await s3DeleteFile({
+        bucketName: config.S3_BUCKET_NAME,
+        key: `product/${prodId}/${img}.png`,
+    })
+
+    
+    const productDeleted = await product.findByIdAndDelete({prodId})
+
+    res.status(202).json({
+        success: true,
+        message: "Product deleted successfully :)",
+        productDeleted,
+        imgDelete
+    })
+}
+
+catch(err){
+    res.status(402).json({
+        success: false,
+        message: err.message
+    })
+
+}
+
+}
+
+exports.searchProduct = async(req, res) => {
+
+    try{
+        const {search} = req.body;
+
+        const searchProduct = await product.find({$or: {name: new RegExp(search, 'i')} ,$or: {brand: new RegExp(search, 'i')}})
+        // const searchpro = await product.aggregate({$search: {name: regexp(search, 'i')}})
+        console.log(searchProduct);
+
+        const filteredProd = searchProduct.filter(product)
+        console.log(filteredProd + "product Filtered");
+
+
+        res.status(202).json({
+            success: true,
+            message: "Here is the product you searched for",
+            filteredProd
+        })//
+
+    }
+
+    catch(err){
+
+        res.status(404).json({
+            success: true,
+            message: err.message
+        })
+    }
+}
+
---
+++ controllers/userController.js
@@ -0,0 +1,276 @@
+import  {user} from "../model/userSchema";
+// const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+export const cookieProp = {
+    expires: new Date(Date.now(2*24*60*60*1000)),
+    httpOnly: true
+}
+
+exports.createUser = async(req, res) => {
+
+    try{
+        const {name, email, password, phoneNo} = req.body;
+
+        if(!name || !email || !password || !phoneNo){
+            throw new Error("Please enter your Name, Email, Password, PhoneNo")
+
+        }
+
+        // To check if user/email already exist
+
+        const existedUser = await user.findOne({email})
+        if(existedUser){
+            throw new Error("User with entered Email already exist")
+        }
+
+        const encryptedpswd = bcrypt.hash(password, 13);
+
+        const userCreated = await user.create({
+            name,
+            email,
+            encryptedpswd,
+            phoneNo
+        })
+        
+        console.log(userCreated);
+
+        // Creating token for every new user/D
+
+        const token = userCreated.tokenGenerate();
+
+        userCreated.token = token;
+        userCreated.password = undefined;
+        // setting up the token to cookies
+        res.cookie("token", token, cookieProp)
+
+        res.status(200).json({
+            success: true,
+            message: "User created successfully",
+            userCreated
+
+        })
+
+
+    }
+
+    catch(err){
+
+        console.log(err);
+        res.status(400).json({
+            success: false,
+            message: err.message
+        })
+
+    }
+}
+// This should be a restricted route?
+exports.getUser = async(req, res) => {
+
+    try{
+
+    const userFetched = await user.find({});
+    // If we want to get particular user by email or can we ask user using a prompt msg
+
+    const {email} = req.body;
+    if(email){
+        const requestedUser = await user.find({email});
+        console.log(requestedUser);
+
+        res.status(202).json({
+            success: true,
+            message: "Requsted user fetched successfully",
+            requestedUser
+        })
+    }
+    
+    console.log(userFetched);
+    
+
+    res.status(202).json({
+        success: true,
+        message: "Here are list of users",
+        userFetched
+    })
+
+    }
+
+    catch(err){
+
+        console.log(err);
+        res.status(402).json({
+            success: false,
+            message: err.message
+        })
+
+    }
+
+}
+
+exports.userLogin = async(req, res) => {
+
+    try{
+
+        const {email, password} = req.body;
+        const existingPswd = user.password;
+
+        if(!email || !password){
+            throw new Error("Please enter your Email and password")
+        }
+
+        const userLoggedin = await user.findOne({email}).select("+password");
+
+        if(!userLoggedin){
+            throw new Error("Your credentials are invalid... Please try again")
+        }
+
+        const pswdValidated = bcrypt.compare(password, existingPswd);
+
+        if(pswdValidated === true){
+            const token = userLoggedin.tokenGenerate();
+            userLoggedinpassword = undefined;
+            res.cookie("token", token, cookieProp)
+
+            res.status(202).json({
+                success: true,
+                message: "You logged in successfully, Have a good day :)",
+                userLoggedin,
+                token
+            })
+        }
+
+    }
+
+    catch(err){
+        res.status(402).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+// If user wants to change his credentials
+exports.updateUser = async(req, res) => {
+
+    try{
+
+    const {name, email, password,newPswd, confirmPswd, phoneNo} = req.body;
+    const {existingPswd} = user.password;
+    const {userId} = req.params;
+
+    if(!userId){
+        throw new Error("Sorry we could not fetch your ID")
+    }
+
+    if(!email || !name || !password || !phoneNo){
+        throw new Error("Please Enter your Name, Email, Password and PhoneNo");
+    }
+
+    const pswdCompared = bcrypt.compare(password, existingPswd );
+
+    if(pswdCompared === false){
+
+        throw new Error("Sorry your password do not match");
+        
+    }
+    // generate new token
+    // If user wants to change the password
+    if(newPswd && confirmPswd){
+
+    if(newPswd === confirmPswd){
+        const pswdUpdate = await user.findByIdAndUpdate(userId, {password});
+    }
+
+    else{
+        throw new Error("Value of password and confirm password field should be same")
+    }
+}
+    
+    const userUpdated = await user.findByIdAndUpdate(userId, {name, email, password, phoneNo});
+    
+
+    const regenratedToken = userUpdated.tokenGenerate();
+    console.log(userUpdated);
+    console.log(regenratedToken);
+
+    userUpdated.password = undefined;
+
+    res.cookies("token", regenratedToken, cookieProp)
+    res.status(202).json({
+        success: true,
+        message: "User credentials updated successfully",
+        userUpdated
+    })
+
+    }
+
+    catch(err){
+
+        res.status(401).json({
+            success: false,
+            message: err.message,
+        })
+
+    }
+
+}
+
+exports.userLogout = async(req, res) => {
+
+try{
+
+    res.clearCookie("token", {httpOnly: true});
+
+    res.status(202).json({
+        success: true,
+        message: "User Logged Out Successfully :("
+
+    })
+}
+
+catch(err){
+    
+    res.status(402).json({
+        success: false,
+        message: err.message
+    })
+}
+
+}
+
+exports.deleteUser = async (req, res) => {
+
+    try{
+
+        const {email, password} = req.body;
+        const existingpswd = user.password;
+
+        if(!email || !password){
+            throw new Error("Please enter your Email and password")
+        }
+
+        const pswdCompared = bcrypt.compare(password, existingpswd);
+
+        if(pswdCompared === true){
+            const userDeleted = await user.findOneAndDelete({email})
+            console.log(userDeleted);
+            
+            res.status(201).json({
+                success: true,
+                message: "User deleted successfully",
+                userDeleted
+            })
+
+        }
+    }
+
+    catch(err){
+
+        res.status(402).json({
+            success: false,
+            message: err.message,
+        })
+
+    }
+
+}
