8549f12c762f6b3911f122e7775f06bfcbdc86d5
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-09-13 14:01:59 +0530

    Added models

---
+++ model/cartSchema.js
@@ -0,0 +1,22 @@
+import mongoose ,{Schema} from "mongoose";
+
+const cartSchema = mongoose.Schema({
+
+    userId:{
+        type: {type: Schema.Types.ObjectId,
+        ref: "user"}
+
+    },
+
+    product: {
+        type: [{type: Schema.Types.Subdocument,
+        ref: "tshirt"}]
+    },
+
+    quantity: {
+        type: Number,
+        minNumber: [1, "You must have atleast one product"]
+    }
+})
+
+module.exports = mongoose.model("cart", cartSchema)
