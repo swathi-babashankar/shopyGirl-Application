ae0c5d16f961bf5789b40d3004cfb7120a18612e
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-09-22 13:03:05 +0530

    Added orderRoutes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,1 +1,12 @@
-const express = require("express")
+const express = require("express");
+
+const {placeOrder, getAllOrders, editOrderDetails, cancelOrder} = require("../controllers/orderController");
+const router = express.Router();
+
+router.post("/placeOrder/:userId?/:productId", placeOrder);
+router.get("/getAllOrders/:id", getAllOrders);
+router.put("/editOrderDetails/:id?", editOrderDetails);
+router.delete("/cancelOrder/:userId?/:productId?", cancelOrder);
+
+module.exports = router;
+// done
