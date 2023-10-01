cf1b0676ed9e57cefe4af7db3a12055ceb9608b7
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-10-01 12:25:18 +0530

    Added userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,14 @@ const express = require("express");
 const {createUser, getUser, updateUser, deleteUser, userLogin, userLogout} = require("../controllers/userController");
 const router = express.Router();
 
+router.post("/createUser", createUser);
+router.get("/getUser/:adminId", getUser);
+router.put("/updateUser/:id", updateUser);
+router.delete("/deleteUser/:id", deleteUser);
+router.post("/userLogin", userLogin);
+router.get("/userLogout", userLogout);
 
 
 module.exports = router;
+
+// add admin id in getUser so that only admin can see all the users
