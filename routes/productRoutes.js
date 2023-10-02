8be953d0236255b618c2966d1610062b0f17b63c
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-10-02 12:29:25 +0530

    Added productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,15 @@ require("../controllers/productController.js");
 
 const router = express.Router();
 
+router.post("/createProduct/:adminId?", createProduct);
+router.get("/getProducts", getProducts);
+router.get("/getProductById/productId", getProductById);
+router.put("/updateProductById/:adminId?/:productId?", updateProductById);
+// create admin and add adminId make this private doing so
+router.put("/updateProductByBrand/:adminId?", updateProductByBrand);
+router.delete("/deleteProduct/:adminId?/:productId?", deleteProduct);
+router.get("/searchProduct", searchProduct);
 
+module.exports = router;
 
-// module.exports = router;
+// Not completed - Create admin and add adminIds for protected routes
