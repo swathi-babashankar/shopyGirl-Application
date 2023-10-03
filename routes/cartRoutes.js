d0761451227e5ed2b8fec65699587c554a32f2bd
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-10-03 14:55:08 +0530

    Modified and added isLoggedIn method

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {addToCart, getItems, getByUserId, editCart, deleteCartItem} = require("../controllers/cartController");
+const {isLoggedIn} = require("../controllers/userController");
 
-router.post("/addToCart/:productId/:userId?", addToCart);
-router.get("/getItems/:userId", getItems);
-router.get("/getByUserId/:userId", getByUserId);
-router.put("/editCart/:productId/:userId?", editCart);
-router.delete("/deleteCartItem/:productId/:userId?", deleteCartItem);
+router.post("/addToCart/:productId/:userId?",isLoggedIn, addToCart);
+router.get("/getItems/:userId",isLoggedIn, getItems);
+router.get("/getByUserId/:userId",isLoggedIn, getByUserId);
+router.put("/editCart/:productId/:userId?",isLoggedIn, editCart);
+router.delete("/deleteCartItem/:productId/:userId?",isLoggedIn, deleteCartItem);
 
 module.exports = router;
 
