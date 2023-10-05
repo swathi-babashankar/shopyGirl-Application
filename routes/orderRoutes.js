4450b9c5ddc3ee174b81f03679d38269f5e4d3b1
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-10-05 21:11:38 +0530

    Converted to protected routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const {placeOrder, getAllOrders, editOrderDetails, cancelOrder} = require("../controllers/orderController");
+const {isLoggedIn} = require("../controllers/userController");
 const router = express.Router();
 
-router.post("/placeOrder/:userId?/:productId", placeOrder);
-router.get("/getAllOrders/:id", getAllOrders);
-router.put("/editOrderDetails/:id?", editOrderDetails);
-router.delete("/cancelOrder/:userId?/:productId?", cancelOrder);
+router.post("/placeOrder/:userId?/:productId",isLoggedIn, placeOrder);
+router.get("/getAllOrders/:id",isLoggedIn, getAllOrders);
+router.put("/editOrderDetails/:id?",isLoggedIn, editOrderDetails);
+router.delete("/cancelOrder/:userId?/:productId?",isLoggedIn, cancelOrder);
 
 module.exports = router;
-// done
+// done 
