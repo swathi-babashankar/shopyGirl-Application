12e8f02f115fcbe067fc15fb2ab3f4f768179c11
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-10-08 13:38:58 +0530

    Added auth for cartRoutes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {addToCart, getItems, getByUserId, editCart, deleteCartItem} = require("../controllers/cartController");
-const {isLoggedIn} = require("../controllers/userController");
+const {loginAuth} = require("../authentication/userAuth");
 
-router.post("/addToCart/:productId/:userId?",isLoggedIn, addToCart);
-router.get("/getItems/:userId",isLoggedIn, getItems);
-router.get("/getByUserId/:userId",isLoggedIn, getByUserId);
-router.put("/editCart/:productId/:userId?",isLoggedIn, editCart);
-router.delete("/deleteCartItem/:productId/:userId?",isLoggedIn, deleteCartItem);
+router.post("/addToCart/:productId/:userId?",loginAuth, addToCart);
+router.get("/getItems/:userId",loginAuth, getItems);
+router.get("/getByUserId/:userId",loginAuth, getByUserId);
+router.put("/editCart/:productId/:userId?",loginAuth, editCart);
+router.delete("/deleteCartItem/:productId/:userId?",loginAuth, deleteCartItem);
 
 module.exports = router;
 
