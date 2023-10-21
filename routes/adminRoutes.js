0bb72691d3be3c2a793b719c73a5e979c4673b66
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2023-10-21 14:33:07 +0530

    Updated adminRoutes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {createAdmin, editAdmin, adminLogin, adminLogout, deleteAccount} = require("../controllers/adminController");
+const {adminAuth} = require("../authentication/adminAuth");
+
+// check if token is admin token and is loggedin later protect thid with express-jwt 
+// No other user should be able to create admin except me
 
 router.post("/createAdmin", createAdmin);
-router.put("/editAdmin/:id", editAdmin);
-router.get("/adminLogin", adminLogin);
-router.get("/adminLogout", adminLogout);
-router.delete("/deleteAccount/:id", deleteAccount);
+router.put("/editAdmin/:id?",adminAuth, editAdmin);
+router.post("/adminLogin", adminLogin);
+router.get("/adminLogout",adminAuth, adminLogout);
+router.delete("/deleteAccount/:id?",adminAuth, deleteAccount);
 
 module.exports = router;
