73657a2606acb524d31071254eb752bc93be3446
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2024-12-20 22:09:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
               'origin': "*",
-              'methods': ["GET", "POST","PUT", "DELETE"],
-               'preflightContinue': false,
-               'allowedHeaders': ['Content-Type','Authorization'],
-                'exposedHeaders': ['Content-Range','X-Content-Range'],
   credentials: true}));
 
 app.use(function (req, res, next) {
