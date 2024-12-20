d473a3d91b1ff97651b6c72b91d453f55b7cec11
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2024-12-20 21:43:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
   credentials: true}));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  req.header("Access-Control-Allow-Origin", "*");
   next();
 })
 
