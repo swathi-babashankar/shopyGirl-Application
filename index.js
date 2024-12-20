15395d5295cdcbf7b66523cd1154059c59a19533
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2024-12-20 08:10:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const multer = require("multer")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({ origin: ["https://shopyguyapplication.vercel.app"],
+app.use(cors({ origin: ["https://shopy-guys-application-frontend-1pcafr2zw.vercel.app/"],
               methods: ["get", "post", "put", "delete" ],
   credentials: true}));
 app.use(cookieParser());
