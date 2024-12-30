1112a8849c7be0386dc5730664f699d61098ef1f
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2024-12-30 17:01:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const multer = require("multer")
 // const bodyParser = require("body-parser")
 
 app.use(function(req, res){
-  res.setHeader({'Access-Control-Allow-Origin': "*"});
+  res.setHeader({
+    'Access-Control-Allow-Origin': *});
   // res.setHeader("Access-Control-Allow-Credentials", true);
 })
 
