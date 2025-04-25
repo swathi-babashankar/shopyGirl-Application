96f065c13f31f0dc9e2c9d3447846ebdaef2e27c
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-04-25 16:10:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const app = require("./api/index.js");
+const serverless = require("serverless-http");
 const PORT = process.env.PORT|| 4000;
 
-app.listen(PORT, ()=>{
-    console.log(`App is running at http://localhost:${PORT}`);
-})
-;
+// app.listen(PORT, ()=>{
+//     console.log(`App is running at http://localhost:${PORT}`);
+// })
+// ;
+module.exports.handler = serverless(app);
