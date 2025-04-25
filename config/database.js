0d1f777c2fb1d1856822a6f275a205febbd07333
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-04-25 13:41:34 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const connectToDatabase = () => {
-    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+const connectToDatabase = async () => {
+  await  mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((conn)=>{
         console.log(conn.connect,"Connected to DataBase");
     })
