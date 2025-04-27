70d43f879806252e89cbeddc5ce0b3ee9f3d2435
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-04-27 12:48:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ app.use(cors({
   'credentials': true}));
 
 app.use(function(req, res){
-  res.writeHead(200, {'Access-Control-Allow-Origin': 'https://shopyguysapplicationfrontend.vercel.app', 'Access-Control-Allow-Credentials': true})
-  // res.setHeader("Access-Control-Allow-Credentials", true);200 https://shopyguysapplicationfrontend.vercel.app
+  // res.writeHead(200, {'Access-Control-Allow-Origin': 'https://shopyguysapplicationfrontend.vercel.app', 'Access-Control-Allow-Credentials': true})
+  res.setHeader({'Access-Control-Allow-Origin': 'https://shopyguysapplicationfrontend.vercel.app', 'Access-Control-Allow-Credentials': true});
 })
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
