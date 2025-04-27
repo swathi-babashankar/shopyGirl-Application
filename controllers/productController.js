65728f7d1e6903cb9e85a7b75a71473e3f5a5788
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-04-27 14:13:51 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -92,7 +92,7 @@ exports.getProducts = async(req, res) => {
 
     try{
 
-        const getAllprods = await product.find({}).toArray();
+        const getAllprods = await product.find({});
 console.log(getAllprods);
         res.status(202).json({
             success: true,
