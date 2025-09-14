64a2f5322245809e96432b2295319a7316cd4ff9
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-09-14 12:29:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', 'https://shopyguysapplicationfrontend.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT, POST,DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+    res.setHeader('Access-Control-Allow-Credentials', true); 
     res.status(200).end();  // Send a 200 response for OPTIONS request
     return;
   }
