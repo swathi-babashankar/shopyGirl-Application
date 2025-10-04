d11994a25a2b3954f9147ada2224c61480dadc11
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-10-04 13:57:53 +0530

    Update multerMiddleware.js

--- methods/multerMiddleware.js
+++ methods/multerMiddleware.js
@@ -1,19 +1,20 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        // image is not showing in tmp folder after upload in cloudinary
-        cb(null, "/backend/tmp")
-    },
+const storage = multer.memoryStorage()
+//     destination: function(req, file, cb){
+//         // image is not showing in tmp folder after upload in cloudinary
+//         cb(null, "/backend/tmp")
+//     },
 
-    filename: function(req, file, cb){
-        cb(null, file.originalname)
-    }
-})
+//     filename: function(req, file, cb){
+//         cb(null, file.originalname)
+//     }
+// })
 
 exports.upload = multer({
     storage
 
 })
+
 
 
